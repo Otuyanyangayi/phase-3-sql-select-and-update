a92f18908dbd93b6c4dbfd8590eaca23099117e5
Grace <gracenyangayi@gmail.com>
2022-08-17 10:04:52 +0300

    commit tree bf705986cc5f622e94a11ebb5ec0fb0f92f1e770

---
+++ 01_insert_cats_into_cats_table.sql
@@ -0,0 +1,5 @@
+INSERT INTO cats (name, age, breed) VALUES ('Lil Bub', 5, 'American Schorthair');
+
+INSERT INTO cats (name, age, breed) VALUES ('Hannah',1  , 'Tabby');
+
+UPDATE cats SET age = 1 WHERE age = 5;
